9e28155220375f045c8dbc079c2cd7c1da71e006
vortex-prime <53833359+vortex-prime@users.noreply.github.com>
2025-07-24 16:00:50 -0400

    Automated Terraform update - 20250724-160050

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,11 +1,11 @@
 variable "cluster_version" {
   description = "EKS cluster version"
   type        = string
-  default     = "1.27"
+  default     = 1.29
 }
 
 variable "node_group_instance_types" {
   description = "Instance types for node groups"
   type        = list(string)
-  default     = ["t3.medium", "t3.large"]
+  default     = ["t3.large", "t3.xlarge"]
 }
